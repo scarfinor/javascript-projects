49a15e026639130eccd9b235ca41dd492c4f7b5b
scarfinor <scarfinor@gmail.com>
2024-06-10 21:02:45 -0500

    git commit -c

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,3 @@
 const input = require('readline-sync');
-
+let info = input.question("Question text...");
 let name = input.question("Enter your name: ");
--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,5 +1,5 @@
    // This demo shows off comments!
-
+   /*helloThisIsMyFirstComment!
    // console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
@@ -14,3 +14,8 @@ console.log("hello")
 console.log /* hello there
 how are we today */
 // This does not print. great!
+ const input = require('readline-sync');
+ let info = input .question("Please enter your age: ");
+ //The user enters 25.
+
+ console.log(typeof info, info);
