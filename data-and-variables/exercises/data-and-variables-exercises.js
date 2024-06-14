0af1c9394573a81d40f53aca24a06d9f11a210ff
scarfinor <scarfinor@gmail.com>
2024-06-14 17:06:27 -0500

    git commit

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -39,32 +39,3 @@ console.log("end -------------------------------")
 // milesToMoon = 238712.4
 // hoursToMoon = 13.640708571428257
 // daysToMoon = 0.5683628571428571
-
-console.log('ignore----------------')
-
-//booleanConversion
-
-console.log(Boolean('True'))
-console.log(Boolean('TRUE'))
-console.log(Boolean(0))
-console.log(Boolean(1))
-console.log(Boolean(''))
-console.log(Boolean('launchCode'))
-
-console.log('tryme-----------------')
-//checkYourUnderstading
-
-//underWhichConditionalDoesBooleanConvertAStringToTrue?
-//1.OnlyWhenTheStringIs"True".
-
-console.log(Boolean('True'))
-
-console.log('question1-------------')
-//whichOfTheFollowingIsABooleanExpression?SelectAllThatApply.
-
-console.log(Boolean(3 == 4))
-console.log(Boolean(3 + 4 === 7))
-console.log(Boolean('false'))
-console.log(Boolean(3 + 4))
-
-console.log('question2-------------')
