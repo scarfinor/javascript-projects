1ff27d0947b28466276f57c1603944cca0a93f56
scarfinor <scarfinor@gmail.com>
2024-06-15 10:11:06 -0500

    git commit

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -16,15 +16,32 @@ if (crewStatus) {
   }
 
 // b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
-if (computerStatusCode) {
+if (computerStatusCode === 200) {
     console.log('pleaseStamdBy.computerIsRebooting')
-} else {
-    console.log('crewNotReady')
-}
+    } else if (computerStatusCode === 400) {
+    console.log('success!computerOnline.')
+    } else {
+    console.log('ALERT: computerOffline!')
+    }
 
 // c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
-
+if (shuttleSpeed > 17500) {
+    console.log('ALERT: escapeVelocityReached!')
+    } else if (shuttleSpeed <8000) {
+        console.log('ALERT: cannotMaintainOrbit!')
+    } else {
+            console.log('stableSpeed')
+    }
 
 // 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
-
-console.log(/* "Yes" or "No" */);
+if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
+console.log('yes')
