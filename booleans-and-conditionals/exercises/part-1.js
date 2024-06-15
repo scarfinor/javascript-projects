a514627be904861a97b23833be3558c16eea255e
scarfinor <scarfinor@gmail.com>
2024-06-15 09:16:04 -0500

    git commit

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = ('redBlinking') 
+let spaceSuitsOn	= ('true')
+let shuttleCabinReady	= ('true')
+let crewStatus	= (spaceSuitsOn && shuttleCabinReady)
+let computerStatusCode	= (200)
+let shuttleSpeed	= (15000)
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
@@ -9,4 +15,3 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
-console.log('ignore-------------------')
