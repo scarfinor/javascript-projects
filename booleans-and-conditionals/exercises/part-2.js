6fb9bee359a82c6d55270b27adfa2615228e6192
scarfinor <scarfinor@gmail.com>
2024-06-15 09:35:09 -0500

    git commit

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -7,11 +7,20 @@ let shuttleSpeed = 15000;
 
 // 3) Write conditional expressions to satisfy the following safety rules:
 
-// a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
 
+// a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
+if (crewStatus) {
+    console.log('crewReady')
+  } else {
+    console.log('crewNotReady')
+  }
 
 // b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
-
+if (computerStatusCode) {
+    console.log('pleaseStamdBy.computerIsRebooting')
+} else {
+    console.log('crewNotReady')
+}
 
 // c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
 
