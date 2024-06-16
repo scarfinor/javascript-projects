6e5c40b258983d3ce17d001f8ccfbda202508c55
scarfinor <scarfinor@gmail.com>
2024-06-15 21:15:51 -0500

    git commit

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -1,6 +1,6 @@
 // Run this sample code as-is and examine the output. 
-// Should the shuttle have launched? 
-// Did it?
+// Should the shuttle have launched? no
+// Did it? yes
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
 
 let launchReady = false;
