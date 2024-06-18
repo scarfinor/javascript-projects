b7cf5d117da4126700e776c29fbe142b8b6c4579
scarfinor <scarfinor@gmail.com>
2024-06-17 21:25:34 -0500

    git commit FINAL STUDIO 2

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -2,7 +2,7 @@
 let date = 'Monday 2019-03-18';
 let time = '10:05:34 AM';
 let astronautCount = 7;
-let astronautStatus	= ready;
+let astronautStatus	= 'ready';
 let averageAstronautMassKg	= 80.7;
 let crewMassKg	= astronautCount * averageAstronautMassKg;
 let fuelMassKg	= 760000;
@@ -13,24 +13,24 @@ let fuelTempCelsius	= -225;
 let minimumFuelTemp	= -300;
 let maximumFuelTemp	= -150;
 let fuelLevel = 1.0; 
-let weatherStatus = clear;
+let weatherStatus = 'clear';
 let preparedForLiftOff = true;
-console.log('------------------------------------------------');
+console.log('------------------------------------------------------------------');
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 if (astronautCount <= 7) {
     preparedForLiftOff = true
 };
 if (astronautCount = true) {
-    console.log('Cleared for Lift Off')
+    console.log('All astronauts on board: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };  
 // add logic below to verify all astronauts are ready
-if (astronautStatus = ready) {
+if (astronautStatus = 'ready') {
     preparedForLiftOff = true
 };
 if ( astronautStatus = true) {
-    console.log('Cleared for Lift Off')
+    console.log('Astronaut ready: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };
@@ -39,7 +39,7 @@ if (totalMassKg < maximumMassLimit) {
     preparedForLiftOff = true
 };
 if (totalMassKg = true) {
-    console.log('Cleared for Lift Off')
+    console.log('Mass within limit: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };
@@ -48,7 +48,7 @@ if (fuelTempCelsius <= -300 || -150) {
     preparedForLiftOff = true
 };
 if (fuelTempCelsius = true) {
-    console.log('Cleared for Lift Off')
+    console.log('Temperature within range: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };
@@ -57,19 +57,21 @@ if (fuelLevel = 1.0) {
     preparedForLiftOff = true
 };
 if (fuelLevel = true) {
-    console.log('Cleared for Lift Off')
+    console.log('Fuel Levle 100%: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };
 // add logic below to verify the weather status is clear
-if (weatherStatus = clear) {
+if (weatherStatus = 'clear') {
     preparedForLiftOff = true
 };
 if (weatherStatus = true) {
-    console.log('Cleared for Lift Off')
+    console.log('Weather clear: Cleared for Lift Off')
 } else {
     console.log('Scrub Launch')
 };
+console.log('------------------------------------------------------------------');
+console.log('All systems are a go! Initiating space shuttle launch sequence.');
 // Verify shuttle launch can proceed based on above conditions
 console.log('------------------------------------------------------------------');
 console.log(('Date: ') + date);
@@ -78,4 +80,6 @@ console.log(('Astronaut Count: ') + astronautCount);
 console.log(('Crew Mass: ') + crewMassKg);
 console.log(('Fuel Mass: ') + fuelLevel);
 console.log(('Shuttle Mass: ') + shuttleMassKg);
-console.log(('Weather Status: ') + clear);
+console.log(('Weather Status: ') + 'clear');
+console.log('------------------------------------------------------------------');
+console.log('Enjoy Your Flight');
