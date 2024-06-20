508364aa896f83aa4f6a116da4bcb8f754a19967
scarfinor <scarfinor@gmail.com>
2024-06-19 22:10:38 -0500

    git commit Part-one-arrays.js exercise final

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,12 @@
 //Create an array called practiceFile with the following entry: 273.15
-
+let practiceFile = [273.15];
+console.log(practiceFile)
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+practiceFile[0] = 42;
+console.log(practiceFile[0]);
+practiceFile[1] = "hello";
+console.log(practiceFile[1]); 
 
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+practiceFile.push(false, -4.6, "87");
+console.log(practiceFile);
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color =  colorful;
+let pluralNoun = 'operations' ;
+let name = 'us';
+let verb = 'work';
+let adjective = 'special';
+let color =  'rich';
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
