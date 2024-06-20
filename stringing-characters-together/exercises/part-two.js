7fa3b5d44c93184ba7c761f9f06b8e8c2d002143
scarfinor <scarfinor@gmail.com>
2024-06-19 21:03:35 -0500

    git commit final + addition to exerciese

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -5,11 +5,13 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 // First, print out the dna strand in it's current state.
 console.log(dna)
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
-console.log(dna.trim(" "));
+let newString = dna.trim();
+console.log(newString);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
-
-console.log(dna.toUpperCase());
+dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+dna = dna.trim().toUpperCase();
+console.log(dna);
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
@@ -29,8 +31,8 @@ if (dna.indexOf("CAT")) {
     console.log("CAT gene not found");
 }
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
-console.log(dna.slice(17, 20));
+console.log(dna.slice(16, 19));
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 console.log(`The DNA strand is ${dna.length} characters long.`);
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
-console.log(`'taco ${dna.replace(" TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ", 'cat')}'`);
+console.log(`'taco ${dna.replace(" TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    "), 'cat'}'`);
