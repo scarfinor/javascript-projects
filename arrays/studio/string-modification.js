57e715efa4d9533df3d1ff6709a4e46f21497221
scarfinor <scarfinor@gmail.com>
2024-06-20 21:30:46 -0500

    git commit array-testing.js final

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,16 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+ let str2 = str.replace("Lau","").replace("e","eLau");
+ console.log(str2)
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let info = input.question("What is the number of letters that will be relocated: ");
+console.log(info)
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (info < 3) {
+    console.log("number longer than the word.");
+    return false;
+}
