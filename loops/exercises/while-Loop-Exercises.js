9358a51d9c970cd6a6b2bdb2538d4fe1f787dc48
scarfinor <scarfinor@gmail.com>
2024-06-23 09:43:42 -0500

    git commit while-Loop-Exercises.js with "readline"

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,5 +1,4 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-let launchCodeShuttle = [fuelLevel, numberOfAstronauts, altitude];
 
 
 
@@ -8,46 +7,31 @@ let launchCodeShuttle = [fuelLevel, numberOfAstronauts, altitude];
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
   const input = require('readline-sync');
-
-  let fuelLevel = input.question("What is the starting fuel level?");
-    let i = 5000;
-   while (i < 30001) {
-    fuelLevel.push(i);
-    console.log(fuelLevel);
+    let fuelLevel = 0, numAstronauts = 0, altitude = 0;
+    
+    while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)) {
+       fuelLevel = input.question("Enter the starting fuel level: ");
     }
-  
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  let numberOfAstronauts = input.question("What is the number of astronauts?");
-  let j = 0; 
-  while (j < 8) {
-    numberOfAstronauts.push(j);
-    console.log(numberOfAstronauts);
-  }
+ while (numAstronauts <= 7) {
+  numAstronauts = input.question("Enter the number of astronauts on borad: ");
+ }
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-let altitude = 0;
-let fuelStatus = [launchCodeShuttle[0] - 100, launchCodeShuttle[1] + 1, launchCodeShuttle[2] + 50];
-let total = 0;
-let k = numberOfAstronauts(j);
-let l = 0;
-
-while (l < k && altitude < 2000) {
-  total += l;
-  fuelStatus.push(l);
-  if (fuelStatus(l)) {
-    console.log("The shuttle gained an altitude of" + altitude + "km");
-  } if (altitude >= 2000 ) {
-    console.log("Orbit achieved!");
-  } else {
-    console.log("Failed to reach orbit.")
+while (fuelLevel-100*numAstronauts >= 0) {
+  altitude += 50;
+  fuelLevel -= 100*numAstronauts;
   }
-  }
-
-
-
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+let output = `The shuttle gained an altitude of ${altitude} km.`;
+
+if (altitude >= 2000) {
+output += "Orbit achieved!";
+} else {
+  output = "Failed to reach orbit."
+}
