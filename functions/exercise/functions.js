52d68f3956d066db940138f367048502f455b66b
scarfinor <scarfinor@gmail.com>
2024-06-26 14:13:34 -0500

    git commit push functions.js final

--- functions/exercise/functions.js
+++ functions/exercise/functions.js
@@ -1,6 +1,6 @@
 function makeLine(size){
     let line = "";
-    for (let i = 0; i < size; i ++) {
+    for (let i = 0; i < size; i++) {
         line += "#";
      }
      return line;
@@ -9,7 +9,7 @@ console.log(makeLine(5));
 console.log("=====================");
 function makeSquare(size, size) {
     let square = "";
-    for (let i = 0; i < size; i ++) {
+    for (let i = 0; i < size; i++) {
         square += (makeLine(size) + "\n");
     }
     return square.slice(0,-1) + makeRectangle(5);
@@ -18,7 +18,7 @@ console.log(makeSquare(5,5));
 console.log("=====================");
 function makeRectangle(width, height) {
     let rectangle = "";
-    for (let i = 0; i < height; i ++) {
+    for (let i = 0; i < height; i++) {
         rectangle += (makeLine(width) + "\n");
     }
     return rectangle.slice(0, -1);
@@ -34,3 +34,35 @@ function makeDownwardStairs(height) {
 }
 console.log(makeDownwardStairs(5));
 console.log("=====================");
+function makeSpaceLine(numSpaces, numChars) {
+    let spaces = "";
+    let Chars = "";
+    for (let i = 0; i < numSpaces; i++) {
+        spaces += " ";
+     }
+     for(let i = 0; i < numChars; i++) {
+        Chars += "#";
+     }
+     return spaces + Chars + spaces; 
+}
+console.log(makeSpaceLine(3, 5));
+console.log("=====================");
+function maketIsoscelesTriangle(height) {
+    let triangle = "";
+    for (let i =0; i < height; i++) {
+        triangle += (makeSpaceLine(height - i - 1, 2 * i + 1) + "\n");
+    }
+    return triangle.slice(0, -1);
+}
+console.log(maketIsoscelesTriangle(5));
+console.log("=====================");
+function reverse(str) {
+    let reversed = '';
+
+    for (let i = 0; i < str.length; i++) {
+        reversed = str[i] + reversed;
+    }
+    return reversed;
+}
+console.log( `${maketIsoscelesTriangle(5)}\n${reverse(maketIsoscelesTriangle(5))}`);
+console.log("======================");
