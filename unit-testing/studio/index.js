6c2cfcd6bf4f13d8d79a12b3a47e83efa5d0881e
scarfinor <scarfinor@gmail.com>
2024-07-15 22:08:52 -0500

    git commit Chapter 14 Studio final

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,34 @@
 
 let launchcode = {
-  
+  organization: "nonProfit",
+  executive: "Jeff",
+  precentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutPut: function(num){
+    let parts = {
+        2: "Launch",
+        3: "Code",
+        5: "Rocks"
+    }
+    let result = "";
+    for (let modulus in parts){
+        if (num % modulus === 0) {
+            if (modulus == 5 && result.length !== 0){
+               result += " ";
+            }
+            result += parts[modulus];
+        }
+    }
+    if (result.length === 0){
+        return "Rutabagas! That dosen't work.";
+    } else {
+        result += "!";
+        if (result === "Launch Rocks!"){
+            result += " (CRASH!!!!)";
+        }
+        return result;
+    }
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,60 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("The launchcode object", function(){
 
-  // Write your unit tests here!
-  
+  test("should have a property called organization", function(){
+    expect(launchcode.organization).toEqual("nonProfit");
+  });
+
+  test("should have a property called executive director", function(){
+    expect(launchcode.executive).toEqual("Jeff");
+  });
+
+  test("should have a property called precentageCoolEmplooyees", function(){
+    expect(launchcode.precentageCoolEmployees).toEqual(100);
+  });
+
+  test("should have a property called programsOffered with 3 programs", function(){
+    expect(launchcode.programsOffered[0]).toEqual("Web Development");
+    expect(launchcode.programsOffered[1]).toEqual("Data Analysis");
+    expect(launchcode.programsOffered[2]).toEqual("Liftoff");
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
+
+  describe("should have a method called launchOutPut, which", function(){
+
+    test("should return 'Launch!' for numbers evenly divisible by 2", function(){
+      expect(launchcode.launchOutPut(2)).toEqual("Launch!");
+    });
+
+    test("should return 'Code!' for numbers evenly divisible by 3", function(){
+      expect(launchcode.launchOutPut(3)).toEqual("Code!");
+    });
+
+    test("should return 'Rocks!' for numbers evenly divisible by 5", function(){
+      expect(launchcode.launchOutPut(5)).toEqual("Rocks!");
+    });
+
+    test("should return 'LaunchCode!' for numbers evenly divisible by both 2 and 3", function(){
+      expect(launchcode.launchOutPut(6)).toEqual("LaunchCode!");
+    });
+
+    test("should return 'Code Rocks!' for numbers evenly divisible by both 3 and 5", function(){
+      expect(launchcode.launchOutPut(15)).toEqual("Code Rocks!");
+    });
+
+    test("should return 'Launch Rocks! (CRASH!!!!)' for numbers evenly divisible by both 2 and 5", function(){
+      expect(launchcode.launchOutPut(10)).toEqual("Launch Rocks! (CRASH!!!!)");
+    });
+
+    test("should return 'LaunchCode Rocks!' for numbers evenly divisible by both 2, 3 and 5", function(){
+      expect(launchcode.launchOutPut(30)).toEqual("LaunchCode Rocks!");
+    });
+
+    test("should return 'Rutabagas! That Doesn't work.' for numbers  NOT evenly divisible by both 2, 3 and 5", function(){
+      expect(launchcode.launchOutPut(7)).toEqual("Rutabagas! That dosen't work.");
+    });
+  });
+
 });
